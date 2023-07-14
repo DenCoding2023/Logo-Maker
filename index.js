713696aa5b0c602db9423eab274032d5da92c30b
Dennis Luciano <127637377+DenCoding2023@users.noreply.github.com>
2023-07-14 15:31:26 -0400

    added the main doe for the square

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 const fs = require('fs');
 const inquire = require('inquire');
-const {Circle, Polygon, Square} = require('./lib/shapes');
-const questions = require ('./lib/questions');
+const {Circle, Polygon, Square} = require('./main/lib/shapes');
+const questions = require ('./main/lib/questions');
+
+const init = () =>
+    inquirer.prompt(questions)
+        .then((data) => {
+            console.log ("Create svg file...");
+            switch ('${data.shape}') {
+                case 'Square':
+                    console.log('Suqre is being Created......')
+                    const square = new Square(data.fill, data.stroke, data.
+                        strokeWidth, data.text, data.textColor, data.width, data.width)
+        
+                    fs.writeFile("./main/output/logo.svg", square.rendesSqare(), (err) => {
+                        if (err) {
+                            console.error(err);
+                        }  else {
+                            console.log('Congratrularions!!! Square is now creaged!!');
+                    })
+                                                }
+                    });
+                    break:
