56a8d4b354d8e6e7f24ff49948a59e229c7e06ce
Dennis Luciano <127637377+DenCoding2023@users.noreply.github.com>
2023-07-14 19:23:12 -0400

    corrected the code to render circle , triangle, and square

--- index.js
+++ index.js
@@ -1,24 +1,76 @@
 const fs = require('fs');
-const inquire = require('inquire');
-const {Circle, Polygon, Square} = require('./main/lib/shapes');
-const questions = require ('./main/lib/questions');
+const inquirer = require('inquirer');
+const { Circle, Polygon, Square } = require('./main/lib/shapes');
+const questions = require('./main/lib/questions');
 
-const init = () =>
-    inquirer.prompt(questions)
-        .then((data) => {
-            console.log ("Create svg file...");
-            switch ('${data.shape}') {
-                case 'Square':
-                    console.log('Suqre is being Created......')
-                    const square = new Square(data.fill, data.stroke, data.
-                        strokeWidth, data.text, data.textColor, data.width, data.width)
-        
-                    fs.writeFile("./main/output/logo.svg", square.rendesSqare(), (err) => {
-                        if (err) {
-                            console.error(err);
-                        }  else {
-                            console.log('Congratrularions!!! Square is now creaged!!');
-                    })
-                                                }
-                    });
-                    break:
+const init = () => {
+  inquirer.prompt(questions).then((data) => {
+    console.log("Create svg file...");
+    switch (data.shape) {
+      case 'Square':
+        console.log('Square is being created...');
+        const square = new Square(
+          data.fill,
+          data.stroke,
+          data.strokeWidth,
+          data.text,
+          data.textColor,
+          data.width
+        );
+
+        fs.writeFile("./main/output/logo.svg", square.renderSquare(), (err) => {
+          if (err) {
+            console.error(err);
+          } else {
+            console.log('Congratulations! Square is now created!');
+          }
+        });
+        break;
+
+      case 'Circle':
+        console.log('Circle is being created...');
+        const circle = new Circle(
+          data.fill,
+          data.stroke,
+          data.strokeWidth,
+          data.textColor,
+          data.text,
+          data.radius
+        );
+
+        fs.writeFile("./main/output/logo.svg", circle.renderCircle(), (err) => {
+          if (err) {
+            console.error(err);
+          } else {
+            console.log('Congratulations! Circle is now created!');
+          }
+        });
+        break;
+
+      case 'Triangle':
+        console.log('Triangle is being created...');
+        const triangle = new Polygon(
+          data.fill,
+          data.stroke,
+          data.strokeWidth,
+          data.textColor,
+          data.text,
+          data.radius
+        );
+
+        fs.writeFile("./main/output/logo.svg", triangle.renderPolygon(), (err) => {
+          if (err) {
+            console.error(err);
+          } else {
+            console.log('Congratulations! Triangle is now created!');
+          }
+        });
+        break;
+
+      default:
+        console.log('Invalid shape entered.');
+    }
+  });
+};
+
+init();
